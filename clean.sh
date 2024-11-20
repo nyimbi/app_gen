3b1160a68a26b9989447168aba5b7955d8f69d10
Nyimbi Odero <nyimbi@pm.me>
2024-11-20 17:17:35 +0300

    cleaned up

---
+++ clean.sh
@@ -0,0 +1,5 @@
+git filter-repo --path n_src/blprint_generator/config.yaml --invert-paths
+git filter-repo --path n_src/blprint_generator/config.yaml --invert-paths --force
+git filter-repo --path n_src/vmixins/old/sys_gen_old.py --invert-paths
+git filter-repo --path n_src/vmixins/sys_dev.py --invert-paths
+git filter-repo --path n_src/vmixins/sys_gen.py  --invert-paths
